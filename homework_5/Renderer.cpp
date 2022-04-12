82e09288ff6705a1d3014fecd7d877bd25f0f205
13986874732 <2879363960@qq.com>
2022-04-12 21:37:28 +0800

    fix bug in cpp 17

--- homework_5/Renderer.cpp
+++ homework_5/Renderer.cpp
@@ -223,8 +223,8 @@ void Renderer::Render(const Scene& scene)
         for (int i = 0; i < scene.width; ++i)
         {
             // generate primary ray direction
-            float x;
-            float y;
+            float x = 0.0;
+            float y = 0.0;
             // TODO: Find the x and y positions of the current pixel to get the direction
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
@@ -237,7 +237,8 @@ void Renderer::Render(const Scene& scene)
     }
 
     // save framebuffer to file
-    FILE* fp = fopen("binary.ppm", "wb");
+    FILE* fp;
+    fopen_s(&fp, "binary.ppm", "wb");
     (void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
     for (auto i = 0; i < scene.height * scene.width; ++i) {
         static unsigned char color[3];
